d3d6398ea4c0a0a3a2a7e79291793a1796a2b058
dmeade3 <dcmstrat@gmail.com>
2016-11-01 21:49:22 -0400

    testing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main
 
     public static void main(String[] args)
     {
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000; i++) {
 
 
             hughMungus();
